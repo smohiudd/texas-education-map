8083a1963fa03d4abbf12df3bf8cd4007acbdaaa
Saadiq Mohiuddin <saadiq.mohiuddin@gmail.com>
2020-11-18 01:14:55 -0700

    ux updates

--- src/App.js
+++ src/App.js
@@ -3,31 +3,57 @@ import mapboxgl from 'mapbox-gl';
 import SideBar from "./SideBarContainer"
 import ReactDOM from 'react-dom';
 import * as tf from '@tensorflow/tfjs';
+import {min,max,range} from 'd3-array';
+import {scaleQuantile} from 'd3-scale';
+import {interpolateInferno} from 'd3-scale-chromatic';
+
+
+let style = (attr) => fetch('https://texas-education.s3-us-west-2.amazonaws.com/2019_rates_v2.min.geojson')
+    .then(response => response.json())
+    .then(data => {
+
+      var maxValue = max(data.features, function (d) { return +d.properties[attr] })
+      var minValue = min(data.features, function (d) { return +d.properties[attr] })
+
+      let stops = range(minValue, maxValue, (maxValue-minValue)/30)
+      
+      let my_data = data.features.map(x => x.properties[attr])
+      
+      let quantile = scaleQuantile()
+        .domain(my_data)
+        .range(range(0.1, 0.98, 0.1))
+
+      let paint = ["interpolate", ["linear"], ["get", attr]]
+
+      for (let stop of stops) {
+        paint = paint.concat(stop, interpolateInferno(quantile(stop))) 
+      };
+
+      return paint
+      
+    });
+
+
 
 let model;
 let default_data;
 // let scores =[]
 
+
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2FhZGlxbSIsImEiOiJjamJpMXcxa3AyMG9zMzNyNmdxNDlneGRvIn0.wjlI8r1S_-xxtq2d-W5qPA';
 
 const csvUrl =
-'https://texas-education.s3-us-west-2.amazonaws.com/default_data.csv';
+'https://texas-education.s3-us-west-2.amazonaws.com/default_data_v2.csv';
 
-let u = tf.tensor1d([3.8757244e+01, 2.3383005e+00, 5.2369898e+04, 1.1495037e+01,
-  7.4352322e+00, 1.1252988e+01, 5.6235461e+00, 4.7844457e+04,
-  4.9755422e+04, 5.3323277e+04, 6.2867398e+00, 3.2471012e+01,
-  3.7395945e-01, 2.9658897e+01, 8.5517073e+00, 1.4846865e+01,
-  5.9074255e+02, 1.5947319e+02, 7.3754135e+01, 3.1339368e+02,
-  3.0423239e+02, 3.6559244e+02, 6.8009603e-01, 5.7097118e-02,
-  1.1472786e-02, 3.3084311e-02, 2.1824974e-01])
+let u = tf.tensor1d([6.5460815e+02, 3.2479367e+02, 8.0763390e+01, 5.0810824e+04,
+  1.1113247e+01, 1.8683820e+02, 5.4065762e+00, 6.8529254e-01,
+  5.8481526e-01, 5.9748985e-02, 1.3731660e-01, 2.9945200e-02,
+  1.8817395e-01])
 
-let s = tf.tensor1d([2.8134167e+02, 1.4098845e+00, 3.1503316e+07, 5.4365643e+01,
-  2.1699398e+01, 4.1067871e+01, 1.4653924e+01, 6.9142848e+07,
-  9.4045168e+07, 8.1127272e+07, 4.1826641e+01, 1.8542195e+02,
-  2.5298369e+00, 1.6023384e+02, 3.5409000e+01, 7.6856813e+00,
-  7.8096984e+04, 2.6095008e+04, 1.0486014e+04, 5.6837340e+04,
-  3.7762898e+04, 5.2971285e+04, 2.1756542e-01, 5.3837039e-02,
-  1.1341161e-02, 3.1989738e-02, 1.7061679e-01])
+let s = tf.tensor1d([2.2707634e+05, 7.4294406e+04, 1.7059588e+04, 3.1223866e+07,
+  1.0382882e+01, 5.1530789e+04, 5.6826550e+01, 1.3408921e+00,
+  2.4280638e-01, 5.6179043e-02, 1.1846075e-01, 2.9048486e-02,
+  1.5276451e-01])
 
 let feature_dict = {
   "teacher_total_full_time_equiv":"Full Time Teachers (Count)", 
@@ -57,12 +83,15 @@ let feature_dict = {
     "Grade_Leve_High School":0,
     "Grade_Leve_Junior High":0,
     "Grade_Leve_Middle":0,
-    "new_rate":"Student Performance (Meets Standards,%)"
+    "new_rate":"Student Performance (Meets Standards,%)",
+    "teacher_experience":"Teacher Experience",
+    "principal_experience": "Principal Experience",
+    "assistant_principal_count": "Assistant Principal Count"
 }
 
 
 const predict = async () => {
-  model = await tf.loadLayersModel('https://texas-education.s3-us-west-2.amazonaws.com/model.json')
+  model = await tf.loadLayersModel('https://texas-education.s3-us-west-2.amazonaws.com/model2/model.json')
 }
 
 const getDefaultData = async () =>{
@@ -83,74 +112,43 @@ const getDefaultData = async () =>{
 
 
 
-let features = ["new_rate","teacher_total_full_time_equiv", 
-  "teacher_total_base_salary",
-  "teacher_1_5_years_full_time_equiv",
-  "teacher_6_10_years_full_time_equiv",
-  "teacher_11_20_years_full_time_equiv",
-  "teacher_20_years_full_time_equiv",
-  "teacher_1_5_years_base_salary",
-  "teacher_6_10_years_base_salary",
-  "teacher_11_20_years_base_salary",
-  "teacher_male_full_time_equiv",
-  "teacher_female_full_time_equiv",
-  "teacher_ba_degree_full_time_equiv",
-  "teacher_ms_degree_full_time_equiv",
-  "teacher_student_ratio",
-  "white",
-  "african_american",
-  "hispanic",
-  "at_risk",
-  "economic_disadvant"]
+let features = ["new_rate",
+"all_students",
+"at_risk",
+"african_american",
+"teacher_total_base_salary",
+"teacher_experience",
+"white",
+"principal_experience",
+"assistant_principal_count"]
 
 let prediction_features = [
-  "teacher_total_full_time_equiv", 
-  "school_admin_total_full_time_equiv",
+  "all_students",
+  "at_risk",
+  "african_american",
   "teacher_total_base_salary",
-  "teacher_1_5_years_full_time_equiv",
-  "teacher_6_10_years_full_time_equiv",
-  "teacher_11_20_years_full_time_equiv",
-  "teacher_20_years_full_time_equiv",
-  "teacher_1_5_years_base_salary",
-  "teacher_6_10_years_base_salary",
-  "teacher_11_20_years_base_salary",
-  "teacher_male_full_time_equiv",
-  "teacher_female_full_time_equiv",
-  "teacher_no_degree_full_time_equiv",
-  "teacher_ba_degree_full_time_equiv",
-  "teacher_ms_degree_full_time_equiv",
-  "teacher_student_ratio",
+  "teacher_experience",
+  "white",
+  "principal_experience",
+  "assistant_principal_count"
 ]
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      modal:false,
       score:[],
       feature: 'new_rate',
       mode: "2019 School Data",
-      "teacher_total_full_time_equiv":0, 
-      "school_admin_total_full_time_equiv":0,
-      "teacher_total_base_salary":0,
-      "teacher_1_5_years_full_time_equiv":0,
-      "teacher_6_10_years_full_time_equiv":0,
-      "teacher_11_20_years_full_time_equiv":0,
-      "teacher_20_years_full_time_equiv":0,
-      "teacher_1_5_years_base_salary":0,
-      "teacher_6_10_years_base_salary":0,
-      "teacher_11_20_years_base_salary":0,
-      "teacher_male_full_time_equiv":0,
-      "teacher_female_full_time_equiv":0,
-      "teacher_no_degree_full_time_equiv":0,
-      "teacher_ba_degree_full_time_equiv":0,
-      "teacher_ms_degree_full_time_equiv":0,
-      "teacher_student_ratio":0,
       "all_students":0,
-      "white":0,
-      "african_american":0,
-      "hispanic":0,
       "at_risk":0,
-      "economic_disadvant":0,
+      "african_american":0,
+      "teacher_total_base_salary":0,
+      "teacher_experience":0,
+      "white":0,
+      "principal_experience":0,
+      "assistant_principal_count":0,
       "Grade_Leve_Elementary":0,
       "Grade_Leve_Elementary/Secondary":0,
       "Grade_Leve_High School":0,
@@ -163,13 +161,18 @@ class App extends React.Component {
     this.handleChangePredictor = this.handleChangePredictor.bind(this)
     this.updatePredictions = this.updatePredictions.bind(this)
     this.updatedPredictionGeometry = this.updatedPredictionGeometry.bind(this)
+    this.modalClose = this.modalClose.bind(this)
+    this.modalOpen = this.modalOpen.bind(this)
 
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+
+    let paint_style = await style(this.state.feature)
+
     this.map = new mapboxgl.Map({
       container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/streets-v11',
+      style: 'mapbox://styles/saadiqm/cjbjougmt08z72rsa7me1duoi',
       center: [-99.027878, 31.319759],
       zoom: 6,
       maxZoom: 12,
@@ -179,61 +182,63 @@ class App extends React.Component {
     this.map.on('load', () => {
 
       this.map.addSource('schools', {
-        'type': 'vector',
-        'url': 'mapbox://saadiqm.bjpfp46f'
+        'type': 'geojson',
+        'data': 'https://texas-education.s3-us-west-2.amazonaws.com/2019_rates_v2.min.geojson'
       });
 
-      this.map.addSource('districts', {
-        'type': 'vector',
-        'url': 'mapbox://saadiqm.4nsi58ow'
-      });
+      // this.map.addSource('districts', {
+      //   'type': 'vector',
+      //   'url': 'mapbox://saadiqm.4nsi58ow'
+      // });
 
       this.map.addSource('prediction', {
         'type': 'geojson',
-        'data': 'https://texas-education.s3-us-west-2.amazonaws.com/2019_rates.geojson'
+        'data': 'https://texas-education.s3-us-west-2.amazonaws.com/2019_rates_v2.min.geojson'
       });
 
+
+      
 
       this.map.addLayer({
         'id': '2019 School Data',
         'type': 'circle',
         'source': 'schools',
-        'source-layer': 'schools2019v2-5et1pd',
+        // 'source-layer': 'schools2019v2-5et1pd',
         // 'minzoom':zoomthreshold-1,
         'paint': {
-          'circle-opacity': 0.4,
-          'circle-color': "purple",
+          'circle-opacity': 1,
+          'circle-color': paint_style,
           'circle-radius': [
-            'interpolate', ['exponential',2], ['zoom'],
-            10,['/',["sqrt", ['get', this.state.feature]],4],
-            14,["sqrt", ['get', this.state.feature]],
-          ]
+            'interpolate', ['linear'], ['zoom'],
+            7,2,
+            14,10,
+          ],
         },
         
       })
 
-      this.map.addLayer({
-        'id': '2019 District Data',
-        'type': 'fill',
-        'source': 'districts',
-        'source-layer': '2019_districts-0ch1eu',
-        // 'maxzoom':zoomthreshold,
-        "layout":{
-          "visibility":"none"
-        },
-        'paint': {
-          'fill-color': [
-            'interpolate',
-            ['linear'],
-            ['get', 'teacher_1_5_years_full_time_equiv'],
-            0,
-            'red',
-            100,
-            'blue'
-            ],
-          'fill-opacity': 0.5,
-        }
-      })
+      // this.map.addLayer({
+      //   'id': '2019 District Data',
+      //   'type': 'fill',
+      //   'source': 'districts',
+      //   'source-layer': '2019_districts-0ch1eu',
+      //   // 'maxzoom':zoomthreshold,
+      //   "layout":{
+      //     "visibility":"none"
+      //   },
+      //   'paint': {
+      //     'fill-color': [
+      //       'interpolate',
+      //       ['linear'],
+      //       ['get', 'teacher_1_5_years_full_time_equiv'],
+      //       0,
+      //       'red',
+      //       100,
+      //       'blue'
+      //       ],
+      //     'fill-opacity': 0.5,
+      //   }
+      // })
 
       this.map.addLayer({
         'id': 'Predict Student Performance',
@@ -244,13 +249,13 @@ class App extends React.Component {
         },
         // 'maxzoom':zoomthreshold,
         'paint': {
-          'circle-opacity': 0.5,
-          'circle-color': "purple",
+          'circle-opacity': 0.9,
+          'circle-color': paint_style,
           'circle-radius': [
-            'interpolate', ['exponential',3], ['zoom'],
-            7,['/',["sqrt", ['get', 'new_rate']],4],
-            11,['*',["sqrt", ['get', 'new_rate']],2],
-          ]
+            'interpolate', ['linear'], ['zoom'],
+            7,2,
+            14,10,
+          ],
         },
       })
 
@@ -267,7 +272,6 @@ class App extends React.Component {
 
     getDefaultData()
     predict()
-
     
 
   }
@@ -335,14 +339,24 @@ class App extends React.Component {
   }
 
 
+  modalOpen() {
+    this.setState({ modal: true });
+  }
+
+  modalClose() {
+    this.setState({modal: false});
+  }
+
   popupEnter(e){
     this.map.getCanvas().style.cursor = 'pointer';
 
       var coordinates = e.features[0].geometry.coordinates.slice();
       var description = e.features[0].properties[this.state.feature];
+      var name = e.features[0].properties["School_Nam"];
 
       let text = <div className="prose">
-      <p className="txt-h5 txt-bold">{feature_dict[this.state.feature]}</p>
+      <p className="txt-h5 txt-bold">{name}</p>
+      <p className="txt-h6 txt-bold">{feature_dict[this.state.feature]}</p>
       <p className="txt-l">{description}</p>
       </div>
 
@@ -365,48 +379,56 @@ class App extends React.Component {
     e.preventDefault();
 
     this.setState({feature:e.target.value}, () => {
+      console.log(this.state.feature)
 
-      let paint_property = null
+      style(this.state.feature)
+        .then(paint_property => {
+          this.map.setPaintProperty(this.state.mode, 'circle-color', paint_property);
+        })
 
-      if(this.state.mode==="2019 School Data" || this.state.mode==="Prediction" ){
 
-        if(this.state.feature.includes("salary")){
-          paint_property = [
-            'interpolate', ['exponential',2], ['zoom'],
-            10,['/',['get', this.state.feature],10000],
-            14,['/',['get', this.state.feature],10000],
-          ]
-        }else if(this.state.feature.includes("equiv")){
-          paint_property = [
-            'interpolate', ['exponential',2], ['zoom'],
-            10,['*',['get', this.state.feature],0.1],
-            14,['*',['get', this.state.feature],1],
-          ]
-        }else if(this.state.feature==="teacher_student_ratio"){
-          paint_property = [
-            'interpolate', ['exponential',3], ['zoom'],
-            7,['/',['get', this.state.feature],7],
-            11,['*',['get', this.state.feature],1.5],
-          ]
-        }else if(this.state.feature==="all_students"){
-          paint_property = [
-            'interpolate', ['exponential',2], ['zoom'],
-            5,['/',["sqrt", ['get', this.state.feature]],7],
-            14,['/',["sqrt", ['get', this.state.feature]],2],
-          ]
-        }else{
-          paint_property = [
-            'interpolate', ['exponential',3], ['zoom'],
-            7,['/',["sqrt", ['get', this.state.feature]],5],
-            11,['*',["sqrt", ['get', this.state.feature]],1.5],
-          ]
-        }
 
-      }else{
+      // let paint_property = null
 
-      }
+      // if(this.state.mode==="2019 School Data" || this.state.mode==="Prediction" ){
 
-      this.map.setPaintProperty(this.state.mode, 'circle-radius', paint_property);
+      //   if(this.state.feature.includes("salary")){
+      //     paint_property = [
+      //       'interpolate', ['exponential',2], ['zoom'],
+      //       10,['/',['get', this.state.feature],10000],
+      //       14,['/',['get', this.state.feature],10000],
+      //     ]
+      //   }else if(this.state.feature.includes("equiv")){
+      //     paint_property = [
+      //       'interpolate', ['exponential',2], ['zoom'],
+      //       10,['*',['get', this.state.feature],0.1],
+      //       14,['*',['get', this.state.feature],1],
+      //     ]
+      //   }else if(this.state.feature==="teacher_student_ratio"){
+      //     paint_property = [
+      //       'interpolate', ['exponential',3], ['zoom'],
+      //       7,['/',['get', this.state.feature],7],
+      //       11,['*',['get', this.state.feature],1.5],
+      //     ]
+      //   }else if(this.state.feature==="all_students"){
+      //     paint_property = [
+      //       'interpolate', ['exponential',2], ['zoom'],
+      //       5,['/',["sqrt", ['get', this.state.feature]],7],
+      //       14,['/',["sqrt", ['get', this.state.feature]],2],
+      //     ]
+      //   }else{
+      //     paint_property = [
+      //       'interpolate', ['exponential',3], ['zoom'],
+      //       7,['/',["sqrt", ['get', this.state.feature]],5],
+      //       11,['*',["sqrt", ['get', this.state.feature]],1.5],
+      //     ]
+      //   }
+
+      // }else{
+
+      // }
+
+      // this.map.setPaintProperty(this.state.mode, 'circle-radius', paint_property);
 
     })
     // this.map.setPaintProperty('districts', 'fill-color', [
@@ -464,6 +486,9 @@ class App extends React.Component {
           features = {features}
           prediction_features = {prediction_features}
           onChangePredictor = {this.handleChangePredictor}
+          status = {this.state.modal}
+          handleClose = {this.modalClose}
+          handleOpen = {this.modalOpen}
         />
         <div ref={el => this.mapContainer = el} className='mapContainer' />
       </div>
--- src/SideBarContainer.js
+++ src/SideBarContainer.js
@@ -30,7 +30,10 @@ let feature_dict = {
       "Grade_Leve_High School":0,
       "Grade_Leve_Junior High":0,
       "Grade_Leve_Middle":0,
-      "new_rate":"Student Performance (Meets Standards,%)"
+      "new_rate":"Student Performance (Meets Standards,%)",
+      "teacher_experience":"Teacher Experience",
+    "principal_experience": "Principal Experience",
+    "assistant_principal_count": "Assistant Principal Count"
 }
 
 // function SelectFeature(props){
@@ -62,7 +65,7 @@ function SelectMode(props){
       }
   
     return(
-        <div class='flex-parent flex-parent--center-main'>
+        <div className='flex-parent flex-parent--center-main'>
             <div className='toggle-group'>
                 {options.map(renderOptions)}
             </div>
@@ -88,7 +91,7 @@ function UserInput(props){
     if(props.SelectedMode==="2019 School Data" ||props.SelectedMode==="2019 District Data"  ){
         return(
             
-            <div className="inline-block mr6">
+            <div className="inline-block">
                  <p className="txt-s txt-em">Select a variable to change the data displayed on the map.</p>
                
                 <div className="select-container">
@@ -96,6 +99,23 @@ function UserInput(props){
                         {optionItems}
                     </select>
                     <div className="select-arrow"></div>
+                </div>
+
+                <div className='round shadow-darken10 px12 py12 txt-s'>
+                    <div className='grid mb6'>
+                        <div className='col bg-blue-faint h12'></div>
+                        <div className='col bg-blue-light h12'></div>
+                        <div className='col bg-blue h12'></div>
+                        <div className='col bg-blue-dark h12'></div>
+                        <div className='col bg-red-faint h12'></div>
+                        <div className='col bg-red-light h12'></div>
+                        <div className='col bg-red h12'></div>
+                        <div className='col bg-red-dark h12'></div>
+                    </div>
+                    <div className='grid txt-xs'>
+                        <div className='col flex-child--grow'>Low</div>
+                        <div className='col flex-child--grow align-r'>High</div>
+                    </div>
                 </div>
             </div>
         )
@@ -114,34 +134,71 @@ function UserInput(props){
     
 }
 
+function Modal({ handleClose, show, children }){
+
+    const showHideClassName = show ? "block" : "none";
+
+    return(
+        <div class='flex-parent flex-parent--center-main pt36 z5'>
+                <div class={`shadow-darken50 flex-child bg-white round relative w600 z5 ${showHideClassName}`}>
+                <button class='absolute top right px12 py12 z5' onClick={handleClose}>
+                    X
+                </button>
+                <div class='px24 py24 z5'>
+                    <div class='txt-h2 mb12 z5'>Texas Education Agency Student Performance Prediction</div>
+                    <div class='txt-m z5'>
+                    I am some modal body content.
+                    </div>
+                </div>
+                </div>
+            </div>
+    )
+
+}
+
 
 function SideBar(props) {
     return (
-        <div>
-        <div className='sidebarStyle prose px12 py12'>
 
-            <h3>Texas Education Data Map</h3>
-            <p className="txt-h5">Visualizing and Predicting student performance using historical achievement data from Texas Education Agency</p>
+        <>
+            <div>
 
-
-            <SelectMode checkedValue={props.view} handleViewChange={props.onViewChange}/>
-            {/* <SelectFeature featureValue={props.feature} handleFeatureChange={props.onFeatureChange}/> */}
-
-           
-            <UserInput 
-                SelectedMode = {props.view} 
-                featureValue={props.feature} 
-                handleFeatureChange={props.onFeatureChange}
-                features = {props.features}
-                prediction_features = {props.prediction_features}
-                ChangePredictor = {props.onChangePredictor}
-            />
+        
+                    <Modal show={props.status} handleClose={props.handleClose}></Modal>
 
 
-           
-        </div>
+               
+    
+                <div className='sidebarStyle prose px12 py12'>
 
-        </div>
+                    <button onClick={props.handleOpen} className='btn btn--s bg-green-faint color-green'>About This Project</button>
+
+                    <h3>Texas Education Data Map</h3>
+                    <p className="txt-h5">Visualizing and Predicting student performance using historical achievement data from Texas Education Agency</p>
+
+
+                    <SelectMode checkedValue={props.view} handleViewChange={props.onViewChange}/>
+                    {/* <SelectFeature featureValue={props.feature} handleFeatureChange={props.onFeatureChange}/> */}
+
+                
+                    <UserInput 
+                        SelectedMode = {props.view} 
+                        featureValue={props.feature} 
+                        handleFeatureChange={props.onFeatureChange}
+                        features = {props.features}
+                        prediction_features = {props.prediction_features}
+                        ChangePredictor = {props.onChangePredictor}
+                    />
+
+
+            
+                </div>
+
+            </div>
+
+            </>
+     
+
 
     )
 }
